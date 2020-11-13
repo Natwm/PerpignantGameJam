3d17d52acb465a470c7cb4dd7f8210160117e33f
natwPine <natw.mezza@hotmail.fr>
2020-11-13 20:43:07 +0100

    [feature] PlayerMovement

---
+++ PerpignanGJ/Assets/Scripts/ZoneBehaviours.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneBehaviours : MonoBehaviour
+{
+
+    public enum ObjectNeeded
+    {
+        None
+    }
+
+
+    [SerializeField] private float decreaseSpeed = .5f;
+    [SerializeField] private float zoneLifeTime = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(zoneLifeTime > 0)
+        {
+            zoneLifeTime -= decreaseSpeed * Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Death");
+        }
+    }
+}
